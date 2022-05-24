137a3d53139412d6bc76e167698f721926480f37
thywillbedone <dfmartinez8909@gmail.com>
2022-05-24 20:19:31 +0200

    first commit

---
+++ src/main/java/dwsc/proyecto/movieplayer/MovieplayerApplication.java
@@ -0,0 +1,13 @@
+package dwsc.proyecto.movieplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieplayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieplayerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dwsc/proyecto/movieplayer/MovieplayerApplicationTests.java
@@ -0,0 +1,13 @@
+package dwsc.proyecto.movieplayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieplayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
